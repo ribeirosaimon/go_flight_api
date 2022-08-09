44d764e4b74aa439efed637a7fa6358d9fa57cc1
Saimon Ribeiro <saimonribeiros@hotmail.com>
2022-08-09 17:13:33 -0300

    create mongo

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/ribeirosaimon/go_flight_api/src/config"
+	"github.com/ribeirosaimon/go_flight_api/src/database"
 	"github.com/ribeirosaimon/go_flight_api/src/routers"
 )
 
 func main() {
-	config.LoadConfig()
+	database.NewMongoConnect()
 	fmt.Println(config.StringConn)
 	app := fiber.New()
 	routers.ConfigRouter(app)
